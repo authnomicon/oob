96aac13f68357d9db8b854a4df81e67fbe3771aa
Jared Hanson <jaredhanson@gmail.com>
2024-02-16 10:57:46 -0800

    Stub out out-of-band initiate handler.

---
+++ com/handlers/initiate.js
@@ -0,0 +1,19 @@
+exports = module.exports = function(store) {
+  
+  function initiate(req, res, next) {
+    console.log('transmit code....');
+    console.log(req.body);
+  }
+  
+  
+  return [
+    require('body-parser').urlencoded({ extended: false }),
+    require('csurf')({ value: function(req){ return req.body && req.body.csrf_token; } }),
+    require('flowstate')({ store: store }),
+    initiate
+  ];
+};
+
+exports['@require'] = [
+  'module:flowstate.Store'
+];
--- com/service.js
+++ com/service.js
@@ -1,9 +1,10 @@
 // Module dependencies.
 var express = require('express');
 
-exports = module.exports = function(promptHandler) {
+exports = module.exports = function(promptHandler, initiateHandler) {
   var router = express.Router();
   router.get('/', promptHandler);
+  router.post('/', initiateHandler);
   
   return router;
 };
@@ -12,5 +13,6 @@ exports = module.exports = function(promptHandler) {
 exports['@implements'] = 'http://i.bixbyjs.org/http/Service';
 exports['@path'] = '/login/oob';
 exports['@require'] = [
-  './handlers/prompt'
+  './handlers/prompt',
+  './handlers/initiate'
 ];
