a45fae910b78f96d49f2965458790096a60399d1
Jared Hanson <jaredhanson@gmail.com>
2024-03-04 17:04:39 -0800

    Silence verbose logging.

--- com/handlers/initiate.js
+++ com/handlers/initiate.js
@@ -1,8 +1,6 @@
 exports = module.exports = function(gateway, Address, store) {
   
   function initiate(req, res, next) {
-    console.log('initiate oob auth...');
-    
     // TODO: error if no address
     
     var address = req.body.address
@@ -10,16 +8,11 @@ exports = module.exports = function(gateway, Address, store) {
     
     var addr = Address.parse(address);
     
-    console.log(addr);
-    
-    
     try {
       var self = this;
       
       gateway.challenge(addr.address, transport, addr.scheme, function(err, ctx) {
-        console.log('gateway challenged!!');
-        console.log(err);
-        console.log(ctx);
+        if (err) { return cb(err); }
         
         var state ={};
         state.channel = addr.scheme;
@@ -28,11 +21,6 @@ exports = module.exports = function(gateway, Address, store) {
         state.secret = ctx.secret;
         req.pushState(state, '/login/oob/verify');
         res.redirect('/login/oob/verify');
-        
-        //req.pushState(state, self._verifyURL, function(err, h) {
-        
-        
-        //return cb(err, ctx);
       });
     } catch (ex) {
       console.log("EX!")
