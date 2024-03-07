f3ab89e810fde46f5ded69b67953d9561ac47e9e
Jared Hanson <jaredhanson@gmail.com>
2024-03-06 17:22:34 -0800

    Update JSDoc.

--- com/service.js
+++ com/service.js
@@ -8,30 +8,15 @@ var express = require('express');
  * authenticator.
  *
  * @param {express.RequestHandler} promptHandler - Handler which prompts the
- *          user to enter an address to be authenticated via an out-of-band
- *          device.
- * @param {express.RequestHandler} initiateHandler - Handler which initiates
- *          out-of-band authentication via a device associated with a given
- *          address.
- * @param {express.RequestHandler} inputHandler - Handler which prompts the user
- *          to input a code received by the out-of-band device.
+ *          user to authenticate using an out-of-band device.
  * @param {express.RequestHandler} verifyHandler - Handler which verifies the
- *          code received by the out-of-band device.
+ *.         user's authentication via an an out-of-band device.
  * @returns {express.Router}
  */
 exports = module.exports = function(promptHandler, verifyHandler) {
   var router = express.Router();
-  //router.get('/', promptHandler);
-  //router.post('/', initiateHandler);
   router.get('/', promptHandler);
   router.post('/', verifyHandler);
-  
-  //router.get('/verify', inputHandler);
-  //router.post('/verify', verifyHandler);
-  // TODO: /confirm, transferHandler  Handler which displays the code for the user
-  //.       to transfer to the out-of-band-device.
-  // TODO: /confirm, confirmHandler Handler which confirms that authentication has
-  //.       been completed on the secondary channel
   
   return router;
 };
