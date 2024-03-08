40b7ae225f366d7ebb009d82e60d5cd9f08448f1
Jared Hanson <jaredhanson@gmail.com>
2024-03-08 13:15:45 -0800

    Make scheme yield oobUser, provision in req handler.

--- com/scheme.js
+++ com/scheme.js
@@ -1,46 +1,12 @@
 // Module dependencies.
 var Strategy = require('passport-oob');
 
-var defer = typeof setImmediate === 'function'
-  ? setImmediate
-  : function(fn){ process.nextTick(fn.bind.apply(fn, arguments)); };
-
-exports = module.exports = function(addressStoreFactory, Address) {
+exports = module.exports = function() {
   
-  return new Strategy({
-    verifyURL: '/login/oob/verify'
-  }, function verify(address, transport, channel, cb) {
-    
-    console.log('verify oob...');
-    console.log(address);
-    console.log(transport);
-    console.log(channel);
-    
-    addressStoreFactory.create(channel)
-      .then(function(store) {
-      }, function(err) {
-        defer(cb, err);
-      });
-    
-    
-    /*
-    addressStore.find(address, channel, function(err, user) {
-      console.log('FOUND USER');
-      console.log(err);
-      console.log(user);
-      
-      if (!user) {
-        
-      } else {
-        return cb(null, user);
-      }
-      
-    });
-    */
+  return new Strategy(function(address, transport, channel, cb) {
+    return cb(null, { channel: channel, address: address }, { transport: transport });
   });
 };
 
 // Module annotations.
-exports['@require'] = [
-  'module:@authnomicon/credentials.OOBAddressStoreFactory'
-];
+exports['@require'] = [];
--- test/scheme.test.js
+++ test/scheme.test.js
@@ -10,21 +10,27 @@ var Strategy = require('passport-oob');
 describe('scheme', function() {
   
   it('should verify address', function(done) {
-    var passwords = new Object();
-    passwords.verify = sinon.stub().yieldsAsync(null, true);
-    //var users = new Object();
-    //users.find = sinon.stub().yieldsAsync(null, { id: '248289761001', displayName: 'Jane Doe' });
-    
     var StrategySpy = sinon.spy(Strategy);
     var factory = $require('../com/scheme',
       { 'passport-oob': StrategySpy });
     
-    var scheme = factory(passwords);
+    var scheme = factory();
     expect(StrategySpy).to.have.been.calledOnce;
     expect(scheme).to.be.an.instanceOf(Strategy);
     
-    done();
-    
-  });
+    var verify = StrategySpy.args[0][0];
+    verify('+1-201-555-0123', 'sms', 'tel', function(err, user, info) {
+      if (err) { return done(err); }
+      
+      expect(user).to.deep.equal({
+        channel: 'tel',
+        address: '+1-201-555-0123'
+      });
+      expect(info).to.deep.equal({
+        transport: 'sms'
+      });
+      done();
+    });
+  }); // should verify address
   
 });
