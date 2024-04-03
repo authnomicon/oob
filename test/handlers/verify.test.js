d3ff8813a7607c02aed7a969e2b950775aa48b16
Jared Hanson <jaredhanson@gmail.com>
2024-04-03 11:43:24 -0700

    Add tests.

--- test/handlers/verify.test.js
+++ test/handlers/verify.test.js
@@ -111,6 +111,67 @@ describe('handlers/verify', function() {
         .listen();
     }); // should provision user and log in
     
+    it('should login previously provisioned user', function(done) {
+      var builder = sinon.stub().returns({
+        phoneNumbers: [ { value: '+1-201-555-0123' } ]
+      });
+      var builderFactory = new Object();
+      builderFactory.create = sinon.stub().resolves(builder);
+      var store = new Object();
+      store.find = sinon.stub().yieldsAsync(null, {
+        id: '703887'
+      });
+      store.add = sinon.stub().yieldsAsync(null);
+      var storeFactory = new Object();
+      storeFactory.create = sinon.stub().resolves(store);
+      var directory = new Object();
+      directory.create = sinon.spy();
+      directory.read = sinon.stub().yieldsAsync(null, {
+        id: '703887',
+        displayName: 'Jane Doe'
+      });
+      var authenticator = new Object();
+      authenticator.authenticate = function(name, options) {
+        return function(req, res, next) {
+          req.oobUser = { channel: 'tel', address: '+1-201-555-0123' };
+          next();
+        };
+      };
+      
+      var handler = factory(builderFactory, storeFactory, directory, undefined, authenticator, noopStateStore);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.login = sinon.stub().yieldsAsync(null);
+          
+          req.method = 'POST';
+          req.body = {
+            code: '123456',
+            csrf_token: '3aev7m03-1WTaAw4lJ_GWEMkjwFBu_lwNWG8'
+          };
+          req.session = {
+            csrfSecret: 'zbVXAFVVUSXO0_ZZLBYVP9ue'
+          };
+          req.connection = {};
+        })
+        .finish(function() {
+          expect(storeFactory.create).to.have.been.calledOnceWith('tel');
+          expect(store.find).to.have.been.calledOnceWith('+1-201-555-0123');
+          expect(directory.read).to.have.been.calledOnceWith('703887');
+          expect(directory.create).to.not.have.been.called;
+          expect(store.add).to.not.have.been.called;
+          expect(this.req.login).to.have.been.calledOnceWith({
+            id: '703887',
+            displayName: 'Jane Doe'
+          });
+          
+          expect(this.statusCode).to.equal(302);
+          expect(this.getHeader('Location')).to.equal('/');
+          done();
+        })
+        .listen();
+    }); // should provision user and log in
+    
     it('should provision user, log in, and resume', function(done) {
       var builder = sinon.stub().returns({
         phoneNumbers: [ { value: '+1-201-555-0123' } ]
