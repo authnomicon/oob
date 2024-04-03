73ab387a5164b39bb5dce8a2fce37c89f88ec734
Jared Hanson <jaredhanson@gmail.com>
2024-04-03 12:56:41 -0700

    Add tests.

--- test/handlers/verify.test.js
+++ test/handlers/verify.test.js
@@ -246,6 +246,56 @@ describe('handlers/verify', function() {
         .listen();
     }); // should provision user, log in, and resume
     
+    // WIP
+    it('should next with error when address store fails to be created', function(done) {
+      var builderFactory = new Object();
+      builderFactory.create = sinon.spy();
+      var store = new Object();
+      store.find = sinon.stub().yieldsAsync(null);
+      store.add = sinon.stub().yieldsAsync(null);
+      var storeFactory = new Object();
+      storeFactory.create = sinon.stub().rejects(new Error('something went wrong'));
+      var directory = new Object();
+      directory.create = sinon.stub().yieldsAsync(null, {
+        id: '703887',
+        displayName: 'Jane Doe'
+      });
+      directory.read = sinon.spy();
+      var authenticator = new Object();
+      authenticator.authenticate = function(name, options) {
+        return function(req, res, next) {
+          req.oobUser = { channel: 'tel', address: '+1-201-555-0123' };
+          next();
+        };
+      };
+      
+      var handler = factory(builderFactory, storeFactory, directory, undefined, authenticator, noopStateStore);
+      
+      chai.express.use(handler)
+        .request(function(req, res) {
+          req.login = sinon.stub().yieldsAsync(null);
+          
+          req.method = 'POST';
+          req.body = {
+            code: '123456',
+            csrf_token: '3aev7m03-1WTaAw4lJ_GWEMkjwFBu_lwNWG8'
+          };
+          req.session = {
+            csrfSecret: 'zbVXAFVVUSXO0_ZZLBYVP9ue'
+          };
+          req.connection = {};
+        })
+        .next(function(err, req, res) {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(err.message).to.equal('something went wrong');
+          
+          expect(builderFactory.create).to.not.have.been.called;
+          
+          done();
+        })
+        .listen();
+    }); // should next with error when address store fails to be created
+    
   }); // handler
   
 });
