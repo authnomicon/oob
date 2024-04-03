9bc97aed506e3103086b3c1a47bf6faaf76b1f45
Jared Hanson <jaredhanson@gmail.com>
2024-04-02 17:05:09 -0700

    Add tests.

--- test/channels/tel/address.test.js
+++ test/channels/tel/address.test.js
@@ -3,26 +3,26 @@
 var expect = require('chai').expect;
 var chai = require('chai');
 var sinon = require('sinon');
-var factory = require('../../../com/channels/mailto/address');
+var factory = require('../../../com/channels/tel/address');
 
 
-describe('channels/mailto/address', function() {
+describe('channels/tel/address', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.deep.equal('module:@authnomicon/oob.AddressParser');
-    expect(factory['@channel']).to.equal('mailto');
+    expect(factory['@channel']).to.equal('tel');
   });
   
   it('should parse address', function() {
     var parse = factory();
     
-    var addr = parse('alice@example.com');
+    var addr = parse('+1-201-555-0123');
     expect(addr).to.deep.equal({
-      scheme: 'mailto',
-      address: 'alice@example.com'
+      scheme: 'tel',
+      address: '+12015550123'
     });
   });
   
-  // TODO: non-email test case
+  // TODO: non-phone test case
   
 });
